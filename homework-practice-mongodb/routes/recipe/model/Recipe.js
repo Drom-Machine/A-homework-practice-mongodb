01e4891c769bcfc6295a7855e50914a168d06d63
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-06-03 03:43:13 -0400

    Updated all HW

---
+++ homework-practice-mongodb/routes/recipe/model/Recipe.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+//----------------------------------------------------------------
+
+const recipeSchema = new mongoose.Schema({
+    RecipeName: {
+        type: String,
+    },
+});
+
+
+module.exports = mongoose.model("recipe", recipeSchema);
